99ec09c81e7bc04833de4d3a10a62897953080f4
Zeldaboy111 <50291059+Zeldaboy111@users.noreply.github.com>
2020-09-01 10:18:09 +0200

    Episode 16 commit

---
+++ Ep16/java/tutorial/zeldaboy111/PlayerListener.java
@@ -0,0 +1,46 @@
+package tutorial.zeldaboy111;
+
+import net.minecraft.server.v1_15_R1.PacketPlayOutWorldParticles;
+import net.minecraft.server.v1_15_R1.ParticleParam;
+import net.minecraft.server.v1_15_R1.ParticleParamItem;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import tutorial.zeldaboy111.YML.ymlHandler;
+
+public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void playerJoinEvent(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+
+        e.setJoinMessage("§6" + p.getPlayerListName() + " §ehas joined!");
+        ymlHandler.setLogin(p);
+    }
+
+    @EventHandler
+    public void inventoryClickEvent(InventoryClickEvent e) {
+        if(e.getClickedInventory() == null) return;
+        String name = e.getView().getTitle();
+
+        if(name.equals("Test Inventory")) CustomInventory.instance.click(e);
+    }
+
+    @EventHandler
+    public void moveEvent(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        Location loc = p.getLocation();
+
+        //p.playSound(loc, Sound.BLOCK_BAMBOO_HIT, 14, 0);
+        p.spawnParticle(Particle.REDSTONE, loc.getX(), loc.getY(), loc.getZ(), 5, 0, 0, 0, new Particle.DustOptions(Color.RED, 1));
+    }
+
+
+}
