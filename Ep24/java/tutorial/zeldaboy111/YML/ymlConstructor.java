a66e740e2166f53affe4325e36750cbb6bc72bfe
Zeldaboy111 <50291059+Zeldaboy111@users.noreply.github.com>
2020-10-27 18:34:49 +0100

    Episode 24 published

---
+++ Ep24/java/tutorial/zeldaboy111/YML/ymlConstructor.java
@@ -0,0 +1,8 @@
+package tutorial.zeldaboy111.YML;
+
+public class ymlConstructor extends YML {
+
+    public ymlConstructor(String name, int id) {
+        super(name, id);
+    }
+}
---
+++ Ep24/java/tutorial/zeldaboy111/scoreboard/IScoreboardManager.java
@@ -0,0 +1,11 @@
+package tutorial.zeldaboy111.scoreboard;
+
+import org.bukkit.entity.Player;
+
+public interface IScoreboardManager {
+
+    void insertPlayer(Player p);
+    void removePlayer(Player p);
+
+
+}
