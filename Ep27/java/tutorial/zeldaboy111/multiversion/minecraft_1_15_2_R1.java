e01e717537362b1b5ff687c5a847804c4cdba7b1
Zeldaboy111 <50291059+Zeldaboy111@users.noreply.github.com>
2020-11-17 14:31:57 +0100

    Episode 27 published

---
+++ Ep27/java/tutorial/zeldaboy111/multiversion/minecraft_1_15_2_R1.java
@@ -0,0 +1,30 @@
+package tutorial.zeldaboy111.multiversion;
+
+import net.minecraft.server.v1_15_R1.ChatComponentText;
+import net.minecraft.server.v1_15_R1.Packet;
+import net.minecraft.server.v1_15_R1.PacketPlayOutPlayerListHeaderFooter;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+
+public class minecraft_1_15_2_R1 implements Minecraft {
+
+    @Override
+    public void sendTablist(Player player, String header, String footer) {
+        try {
+            PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
+            Field h = packet.getClass().getDeclaredField("header");
+            Field f = packet.getClass().getDeclaredField("footer");
+            h.setAccessible(true);
+            f.setAccessible(true);
+
+            h.set(packet, new ChatComponentText(header));
+            f.set(packet, new ChatComponentText(footer));
+
+            (((CraftPlayer)player).getHandle()).playerConnection.sendPacket((Packet)packet);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
