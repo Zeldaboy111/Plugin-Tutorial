203a55e587e6c3cd3a28c41e5b9fcd8927888e6b
Zeldaboy111 <50291059+Zeldaboy111@users.noreply.github.com>
2021-01-12 10:24:30 +0100

    Episode 35 published

---
+++ Ep35/java/me/zeldaboy111/holograms/HoloCmd.java
@@ -0,0 +1,53 @@
+package me.zeldaboy111.holograms;
+
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HoloCmd {
+    private final String prefix = "§6§lHologram§8: §7";
+    private static HoloCmd clazz;
+    private HoloCmd() { }
+    public static HoloCmd getInstance() { return clazz == null ? (clazz = new HoloCmd()) : clazz; }
+
+    public void execute(CommandSender s, String[] args) {
+        if(!(s instanceof Player)) s.sendMessage(prefix + "This command can only be executed by a player.");
+        else if(!s.hasPermission("holo.execute")) s.sendMessage(prefix + "You do not have enough permissions to execute this command.");
+        else checkAndExecuteCommand((Player)s, args);
+    }
+
+    private void checkAndExecuteCommand(Player p, String[] args) {
+        if(args.length < 1) p.sendMessage(prefix + "Incorrect Usage! §e/hologram <create | delete | reload> <id>§7.");
+        else if(args[0].equalsIgnoreCase("reload")) reload(p, (args.length >= 2 ? args[1].toLowerCase() : ""));
+        else if(args[0].equalsIgnoreCase("create")) create(p, (args.length >= 2 ? args[1].toLowerCase() : null));
+        else if(args[0].equalsIgnoreCase("delete")) delete(p, (args.length >= 2 ? args[1].toLowerCase() : null));
+    }
+
+    private void create(Player p, String id) {
+        if(!checkIfCreateIdIsValid(p, id)) return;
+        HologramUtil.getInstance().createHologram(id, p.getLocation().getBlock().getLocation().add(0.5, 0.5, 0.5));
+        p.sendMessage(prefix + "Created the hologram of id §e" + id + "§7.");
+    }
+    private Boolean checkIfCreateIdIsValid(Player p, String id) {
+        if(id == null) p.sendMessage(prefix + "Incorrect Usage! §e/hologram create <id>§7.");
+        else if(HologramUtil.getInstance().checkIfHologramExists(id)) p.sendMessage(prefix + "A hologram with the id §e" + id + " §7already exists.");
+        else return true;
+        return false;
+    }
+
+    private void delete(Player p, String id) {
+        if(!checkIfDeleteIdIsValid(p, id)) return;
+        p.sendMessage(prefix + "WIP");
+    }
+    private Boolean checkIfDeleteIdIsValid(Player p, String id) {
+        if(id == null) p.sendMessage(prefix + "Incorrect Usage! §e/hologram delete <id>§7.");
+        else if(!HologramUtil.getInstance().checkIfHologramExists(id)) p.sendMessage(prefix + "A hologram with the id §e" + id + " §7does not exist.");
+        else return true;
+        return false;
+    }
+    private void reload(Player p, String id) {
+        // no id given -> ""
+        p.sendMessage(prefix + "WIP");
+    }
+
+}
---
+++ Ep35/java/me/zeldaboy111/holograms/Hologram.java
@@ -0,0 +1,109 @@
+package me.zeldaboy111.holograms;
+
+import me.zeldaboy111.storage.Yml;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hologram implements IHologram {
+    private static final double lineSpacing = -0.28;
+    private double spacing;
+
+
+    private String id;
+    private Location location;
+    private String[] text;
+    private ArmorStand[] holograms;
+
+    public Hologram(String id, Location location) {
+        this.id = id;
+        this.location = location;
+        this.spacing = 0.0;
+        initText();
+        killNearbyHolograms();
+        initStands();
+        save();
+
+        // Text, clear systeem voor andere stands met dezelfde naam op dezelfde locatie
+
+    }
+
+    @Override
+    public void show() {
+        if(holograms == null || text == null || holograms.length < 1 || text.length < 1) return;
+        for(int id = 0; id < text.length; id++)
+            if(text[id] != null) showHologramFromId(id);
+    }
+    private void showHologramFromId(int id) {
+        if(checkIfStandExists(id)) return;
+        ArmorStand stand = getNewArmorStand(id);
+        if(stand != null) holograms[id] = stand;
+    }
+    private ArmorStand getNewArmorStand(int id) {
+        if(location == null) return null;
+        ArmorStand stand = (ArmorStand)location.getWorld().spawnEntity(location.clone().add(0.0, spacing, 0.0), EntityType.ARMOR_STAND);
+        stand.setCustomName(text[id]);
+        stand.setInvulnerable(true);
+        stand.setCollidable(false);
+        stand.setArms(false);
+        stand.setSmall(true);
+        stand.setGravity(false);
+        stand.setCustomNameVisible(true);
+        stand.setVisible(false);
+
+        spacing += lineSpacing;
+        return stand;
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+
+    @Override
+    public void moveToLocation() {
+
+    }
+
+    @Override
+    public Location getLocation() {
+        return location.clone();
+    }
+    private void initText() {
+        List<String> textFromFile = Yml.holograms.getData().getStringList("hologram." + id + ".text");
+        text = (textFromFile == null || textFromFile.isEmpty() ? new String[] { id } : new String[textFromFile.size()]);
+        if(text[0] == null || !text[0].equals(id)) {
+            for(int i = 0; i < textFromFile.size(); i++) text[i] = textFromFile.get(i);
+        }
+    }
+    private void killNearbyHolograms() {
+
+    }
+    private void initStands() {
+        if(text == null || text.length <= 0) return;
+        holograms = new ArmorStand[text.length];
+        show();
+    }
+    private void save() {
+
+    }
+
+    private Boolean checkIfStandExists(int id) {
+        if(holograms == null || text == null || holograms.length < id || holograms[id] == null) return false;
+        return !holograms[id].isDead() && holograms[id].getHealth() > 0.0;
+    }
+
+}
